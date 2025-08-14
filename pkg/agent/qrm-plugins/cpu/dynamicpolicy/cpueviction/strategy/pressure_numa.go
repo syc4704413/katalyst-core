f97a79bfdc04b3865f82da894a8aef8dd5abd9bc
siyichao <siyichao@bytedance.com>
2025-08-14 17:23:10 +0800

    0.0 + all taint + no consumer

--- pkg/agent/qrm-plugins/cpu/dynamicpolicy/cpueviction/strategy/pressure_numa.go
+++ pkg/agent/qrm-plugins/cpu/dynamicpolicy/cpueviction/strategy/pressure_numa.go
@@ -233,12 +233,12 @@ func (p *NumaCPUPressureEviction) ThresholdMet(_ context.Context, req *pluginapi
 			ThresholdOperator: pluginapi.ThresholdOperator_GREATER_THAN,
 			MetType:           pluginapi.ThresholdMetType_HARD_MET,
 			EvictionScope:     targetMetric,
-			// Condition: &pluginapi.Condition{
-			// 	ConditionType: pluginapi.ConditionType_NODE_CONDITION,
-			// 	Effects:       []string{string(v1.TaintEffectNoSchedule)},
-			// 	ConditionName: evictionConditionCPUUsagePressure,
-			// 	MetCondition:  true,
-			// },
+			Condition: &pluginapi.Condition{
+				ConditionType: pluginapi.ConditionType_NODE_CONDITION,
+				Effects:       []string{string(v1.TaintEffectNoSchedule)},
+				ConditionName: evictionConditionCPUUsagePressure,
+				MetCondition:  true,
+			},
 			CandidatePods: filteredPods,
 		}, nil
 	}
@@ -254,7 +254,13 @@ func (p *NumaCPUPressureEviction) ThresholdMet(_ context.Context, req *pluginapi
 		ThresholdOperator: pluginapi.ThresholdOperator_GREATER_THAN,
 		MetType:           pluginapi.ThresholdMetType_HARD_MET,
 		EvictionScope:     targetMetric,
-		CandidatePods:     filteredPods,
+		Condition: &pluginapi.Condition{
+			ConditionType: pluginapi.ConditionType_NODE_CONDITION,
+			Effects:       []string{string(v1.TaintEffectNoSchedule)},
+			ConditionName: evictionConditionCPUUsagePressure,
+			MetCondition:  true,
+		},
+		CandidatePods: filteredPods,
 	}, nil
 }
 
