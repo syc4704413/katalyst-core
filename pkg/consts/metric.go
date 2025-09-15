7a9073ec9a1df5d238dc4b72e89c20be27e373d8
siyichao <siyichao@bytedance.com>
2025-09-15 17:56:24 +0800

    borwein input

--- pkg/consts/metric.go
+++ pkg/consts/metric.go
@@ -58,10 +58,18 @@ const (
 	MetricMemActiveFileSystem   = "mem.active.file.system"
 	MetricMemInactiveFileSystem = "mem.inactive.file.system"
 
-	MetricMemDirtySystem            = "mem.dirty.system"
-	MetricMemWritebackSystem        = "mem.writeback.system"
-	MetricMemKswapdstealSystem      = "mem.kswapdsteal.system"
-	MetricMemKswapdstealDeltaSystem = "mem.kswapdsteal.delta.system"
+	MetricMemDirtySystem     = "mem.dirty.system"
+	MetricMemWritebackSystem = "mem.writeback.system"
+
+	MetricMemKswapdstealSystem              = "mem.kswapdsteal.system"
+	MetricMemKswapdstealDeltaSystem         = "mem.kswapdsteal.delta.system"
+	MetricMemVmStatPgStealDirectSystem      = "mem.direct.pgsteal.system"
+	MetricMemVmStatPgStealDirectDeltaSystem = "mem.direct.pgsteal.delta.system"
+	MetricMemVmStatPgScanKswapdSystem       = "mem.kswapd.pgscan.system"
+	MetricMemVmStatPgScanKswapdDeltaSystem  = "mem.kswapd.pgscan.delta.system"
+	MetricMemVmStatPgScanDirectSystem       = "mem.direct.pgscan.system"
+	MetricMemVmStatPgScanDirectDeltaSystem  = "mem.direct.pgscan.delta.system"
+	MetricMemVmStatCompactStallSystem       = "mem.compact.stall.system"
 
 	MetricMemSwapTotalSystem       = "mem.swap.total.system"
 	MetricMemSwapFreeSystem        = "mem.swap.free.system"
@@ -141,6 +149,10 @@ const (
 // System Power and CPUFreq metrics
 const (
 	MetricTotalPowerUsedWatts = "total.power.used.watts"
+	MetricCPUPowerUsedWatts   = "cpu.power.used.watts"
+	MetricMemPowerUsedWatts   = "mem.power.used.watts"
+	MetricFanPowerUsedWatts   = "fan.power.used.watts"
+	MetricHDDPowerUsedWatts   = "hdd.power.used.watts"
 	MetricScalingCPUFreqKHZ   = "scaling.cur.freq.khz"
 )
 
@@ -175,7 +187,10 @@ const (
 	MetricMemAMDL3MissLatencyNuma = "mem.latency.amd.l3.miss"
 	MetricMemFragScoreNuma        = "mem.frag.score.numa"
 
-	MetricCPUUsageNuma = "cpu.usage.numa"
+	MetricCPUUsageNuma          = "cpu.usage.numa"
+	MetricCPUUsageNumaAvg       = "cpu.usage.numa.avg"
+	MetricCPUSchedwaitNumaAvg   = "cpu.schedwait.numa.avg"
+	MetricCPUIOWaitRatioNumaAvg = "cpu.iowait.ratio.numa.avg"
 
 	MetricL3MonNuma = "resctrl.l3mon.numa"
 
@@ -188,6 +203,7 @@ const (
 	MetricTotalPsMemBandwidthNuma  = "mbm.total.ps.numa"
 	MetricLocalPsMemBandwidthNuma  = "mbm.local.ps.numa"
 	MetricVictimPsMemBandwidthNuma = "mbm.victim.ps.numa"
+	MetricMBMMaxBytesPSNuma        = "mbm.max.ps.numa"
 )
 
 // System info metrics
